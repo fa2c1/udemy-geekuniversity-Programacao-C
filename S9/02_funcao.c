4083ed0e4779edf91c2f1c5d2a391c75a4cdedb4
Fernando Coutinho <fa2c1@hotmail.com>
2021-04-24 16:49:07 -0300

    Seção 09 Funções

---
+++ S9/02_funcao.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+//aula 02 escrevendo funções
+// main -> principal
+
+// Estrutura das funções
+/*
+ - tipo de retorno
+ - nome
+ - parâmetros de entrada (opcional)
+ - implementação (corpo da função)
+ - retorno (opcional)
+*/
+
+void mensagem(){
+	printf("Bem-vindo!\n");
+}
+
+int soma(int num1, int num2){
+	int res = num1 + num2;
+	return res;
+}
+
+void proximo_char(char caractere){
+	printf("%c", caractere+1);
+}
+
+int main(){
+	printf("Olá...\n");
+	
+	mensagem();
+	
+	int retorno = soma(4, 6);
+	
+	printf("Retorno = %d\n", retorno);
+
+	printf("Retorno = %d\n", soma(4, 6));
+
+	char cara = 97;
+	// 'a' = 97
+	proximo_char(cara);
+	return 0;
+}
